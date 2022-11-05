03de61ae5f9520537d978d44ee23eb8f91526ed6
mugishap <precieuxmugisha@gmail.com>
2022-11-05 21:53:53 +0200

    [fix]:Delete user fixed

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -123,7 +123,10 @@ exports.searchUsers = async (req, res) => {
 
 exports.deleteUser = async (req, res) => {
     try {
-        const { userId } = req.body
+        const { userId } = req.user
+        const user = await User().findByPk(userId)
+        user.destroy();
+        return res.status(200).json({ message: "User deleted successfully" })
     } catch (error) {
         console.log(error)
         return res.status(500).json({ message: "Internal server error", error: error.message })
